62e2ea31cfc9d4e04d901551efd37f2e0eee7a96
tomoya-ni <148838679+tomoya-ni@users.noreply.github.com>
2023-12-13 17:01:21 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <?php
+  $vegetable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+  print_r ($vegetable_data);
+  ?>  
+</body>
+
+</html>
